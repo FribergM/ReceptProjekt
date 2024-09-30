a55f12f3371626d90e3a157b3030a47c1adcfc96
FribergM <145539853+FribergM@users.noreply.github.com>
2024-09-30 15:51:42 +0200

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,3 @@
-// import { useState, useEffect } from 'react'
-// import './App.css'
-// import RecipeCard from './Components/RecipeCard/RecipeCard';
-
-// function App() {
-  // const [recipes, setRecipes] = useState([])
-
-  // const fetchRecipeData = () => {
-  //   fetch(import.meta.env.VITE_API_URL)
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(data => {
-  //       setRecipes(data)
-  //     })
-  // }
-
-//   useEffect(() => {
-//     fetchRecipeData()
-//   }, [])
-
-//   return (
-//     <>
-//       <div>
-//         {recipes.map((recipe) => (
-//           <RecipeCard
-//             key={recipe._id}
-//             image={recipe.imageUrl}
-//             name={recipe.title}
-//             description={recipe.description}
-//             rating={recipe.avgRating}
-//             categories={recipe.categories}
-//             timeInMins={recipe.timeInMins}
-//           />
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
-
-
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./Components/pages/StartPage";
@@ -50,35 +9,6 @@ import { fetchData } from "./api.js";
 
 
 function App() {
-
-  // const [recipes, setRecipes] = useState([])
-  // const [categories, setCategories] = useState([])
-
-  // const fetchRecipeData = () => {
-  //   fetch(import.meta.env.VITE_API_URL)
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(data => {
-  //       setRecipes(data)
-  //     })
-  // }
-
-  // const fetchCategoryData = () => {
-  //   fetch(import.meta.env.VITE_CATEGORY_URL)
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(data => {
-  //       setCategories(data)
-  //     })
-  // }
-
-  // useEffect(() => {
-  //   fetchRecipeData();
-  //   fetchCategoryData();
-  // }, [])
-
   const [recipes, setRecipes] = useState([])
   const [categories, setCategories] = useState([])
 
@@ -98,7 +28,7 @@ function App() {
      <Router>
       <Routes>
         <Route path="/" element ={<StartPage recipes={recipes} categories={categories}/>} />
-        <Route path="/category/:categoryId" element ={<CategoryPage />} />
+        <Route path="/category/:category" element ={<CategoryPage />} />
         <Route path="/recipe/:recipeId" element ={<RecipesPage />} />
       </Routes>
      </Router>
