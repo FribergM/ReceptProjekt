4a9656ba898643126e7b78930e9a58f0efb9054c
LLLLLLisaa <xianochchu@gmail.com>
2024-09-30 10:42:48 +0200

    update cardContainer.jsx

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 // import RecipeCard from "../RecipeCard/RecipeCard";
+import { useEffect } from "react";
 
 function CardContainer() {
-    return <div>
-        <h1>This is Cardcontainer</h1>
-        {/* <RecipeCard /> */}
+    useEffect(() => {
+        const categoriesURL = import.meta.env.VITE_CATEGORI_URL;
+        const loadCategories = async () => {
+            try {
+                const data = await fetchData(categoriesURL);
+                console.log(data)
+                setCategories(data);
+            } catch (error) {
+                console.error("Loading categories failed:", error);
+            }
+        };
+        loadCategories();
+    }, []);
+    return <div className ="card-container">
+        {categories.map((category,index) => (<div key ={index} className="category-card">
+            <h2>{category.name}</h2>
+        </div>))}
+        
     </div>
 }
 
