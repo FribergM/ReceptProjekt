edeae4ec75cab35dedf5b7d153a96af05beede00
LLLLLLisaa <xianochchu@gmail.com>
2024-09-30 10:14:54 +0200

    try to add recipecard component to container

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+// import RecipeCard from "../RecipeCard/RecipeCard";
 
 function CardContainer() {
     return <div>
         <h1>This is Cardcontainer</h1>
+        {/* <RecipeCard /> */}
     </div>
 }
 
