4f06992ea47ef918ed3f5efa5fe34b7b4f1dd1d3
Petter Lundqvist <116175650+Lndqvist@users.noreply.github.com>
2024-09-30 16:20:35 +0200

    Recipe images

--- src/Components/RecipeCard/RecipeCard.jsx
+++ src/Components/RecipeCard/RecipeCard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './RecipeCard.css';
 
 const RecipeCard = ({ image, name, description, rating, categories, timeInMins }) => {
+  let roundedRating = rating == null ? null : Math.round(rating * 10) / 10;
   return (
     <div className="recipe-card">
-      <img src={image} alt={name} className="recipe-image" />
+      <img src={`${import.meta.env.VITE_PUBLIC_URL}/images/${image}`} alt={name} className="recipe-image" />
       <div className="recipe-info">
         <h2 className="recipe-name">{name}</h2>
         <p className="recipe-description">{description}</p>
         <div className="recipe-meta">
           <div className="recipe-rating">
-            {rating}
+            {roundedRating}
           </div>
           <div className="recipe-time">{timeInMins} min</div>
         </div>
