8ec0f2d710bc9b3a11570e60f85abcf6126c56dc
LLLLLLisaa <xianochchu@gmail.com>
2024-09-30 09:40:03 +0200

    modify path in navabar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import Category from "./Category";
-import Search from "./Search";
-import '../../styles/navbar.css'
+import Category from "../Category/Category";
+import Search from "../Search/Search";
+import './navbar.css'
 
 function NavBar() {
     return <div className = "navbar" style ={{display :"flex",justifyContent:"space-between"}}>
