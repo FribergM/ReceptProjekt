34eb7b08862bba724cd16e94f2452f8842393297
FribergM <145539853+FribergM@users.noreply.github.com>
2024-10-03 18:58:14 +0200

    Made card container responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Search from "../Search/Search";
 import './navbar.css'
 
 function NavBar({categories, recipes}) {
-    return <div className = "navbar" style ={{display :"flex",justifyContent:"space-between"}}>
+    return <div className = "navbar">
              {/* <div className ="hamburger-menu" onClick={() => setShowCatgories(!showCategories)}>
                 <i className ="fas fa-bars"></i>
              </div> */}
