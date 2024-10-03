f315bebe70c23dff65433eb487237360cac92a1f
FribergM <145539853+FribergM@users.noreply.github.com>
2024-10-03 23:27:47 +0200

    Header re-directing to start page.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
     return(
-        <div>
-           <header className = "header">
-              {/* <img src = '../image/headimg.jpeg' alt ="picture" className = "header-image" /> */}
-              <h1 className = "header__name">Mangia</h1>
-           </header>
-        </div>  
+        <Link to={`/`}>
+            <div>
+                <header className = "header">
+                    {/* <img src = '../image/headimg.jpeg' alt ="picture" className = "header-image" /> */}
+                    <h1 className = "header__name">Mangia</h1>
+                </header>
+            </div>  
+        </Link>
     );
 };
 
