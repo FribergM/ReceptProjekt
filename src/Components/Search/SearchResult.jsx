ab93d1a1614fcf9d69340f548d9b3fd34f5946af
LLLLLLisaa <xianochchu@gmail.com>
2024-10-07 17:57:03 +0200

    add condition if search for number

--- src/Components/Search/SearchResult.jsx
+++ src/Components/Search/SearchResult.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import CardContainer from '../CardContainer/CardContainer';
 
- function SearchResult ({recipes, query}) {
-    console.log("SearchResult recipes", recipes)
-    const filterRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(query.toLowerCase()))
 
-    console.log("searchResult", filterRecipes)
+ function SearchResult ({recipes, query}) {
+
+   //  const filterRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(query.toLowerCase()))
+
+    const filterRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(query.toLowerCase()) || recipe.timeInMins == Number(query))
  
 
  return(
     <div className = "searched-recipes">
-        <h2> Search: {query} </h2>
+        {/* <h2> Search: {query} </h2> */}
+        {/* <h2>
+  { !isNaN(query) 
+      ? `Search: ${query}<br /> Visa alla menyer med denna tillagningstid. `  
+      : `Search: ${query}<br />>Visa alla menyer som sina namn innehåller dessa bokstäver.`            
+  }
+</h2> */}
+
+<h3>
+  { !isNaN(query) 
+      ? <>Search: {query}<br />Visa alla menyer med denna tillagningstid.</>  
+      : <>Search: {query}<br />Visa alla menyer som sina namn innehåller dessa bokstäver.</>            
+  }
+</h3>
+
         {/* <CardContainer recipes ={filterRecipes} /> */}
 
         {filterRecipes.length>0 ? <CardContainer recipes ={filterRecipes} /> : "No recipe founded."}
