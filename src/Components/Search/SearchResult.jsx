f728cf127a21028d437cbd3b0e2c9bef10a0aac0
FribergM <145539853+FribergM@users.noreply.github.com>
2024-10-09 21:32:30 +0200

    Fixed bug in responsive grid.

--- src/Components/Search/SearchResult.jsx
+++ src/Components/Search/SearchResult.jsx
@@ -9,30 +9,30 @@ import CardContainer from '../CardContainer/CardContainer';
     const filterRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(query.toLowerCase()) || recipe.timeInMins == Number(query))
  
 
- return(
-    <div className = "searched-recipes category-wrapper">
-        {/* <h2> Search: {query} </h2> */}
-        {/* <h2>
-  { !isNaN(query) 
-      ? `Search: ${query}<br /> Visa alla menyer med denna tillagningstid. `  
-      : `Search: ${query}<br />>Visa alla menyer som sina namn innehåller dessa bokstäver.`            
-  }
-</h2> */}
+   return (
+     <div className="searched-recipes category-wrapper">
+       {/* <h2> Search: {query} </h2> */}
+       {/* <h2>
+        { !isNaN(query) 
+          ? `Search: ${query}<br /> Visa alla menyer med denna tillagningstid. `  
+          : `Search: ${query}<br />>Visa alla menyer som sina namn innehåller dessa bokstäver.`            
+        }
+        </h2> */}
 
-  <div className='category-section'>
-  <h2>Result</h2>
-<h3>
-  { !isNaN(query) 
-      ? <>Search: {query}<br />Visa alla menyer med denna tillagningstid.</>  
-      : <>Search: {query}<br />Visa alla menyer som sina namn innehåller dessa bokstäver.</>            
-  }
-</h3>
+       <div className='category-section'>
+         <h2>Result</h2>
+         <h3>
+           {!isNaN(query)
+             ? <>Search: {query}<br />Visa alla menyer med denna tillagningstid.</>
+             : <>Search: {query}<br />Visa alla menyer som sina namn innehåller dessa bokstäver.</>
+           }
+         </h3>
 
-        {/* <CardContainer recipes ={filterRecipes} /> */}
-        {filterRecipes.length>0 ? <CardContainer recipes ={filterRecipes} /> : "No recipe founded."}
-        </div>
-    </div>
- )
+         {/* <CardContainer recipes ={filterRecipes} /> */}
+         {filterRecipes.length > 0 ? <CardContainer recipes={filterRecipes} /> : "No recipe founded."}
+       </div>
+     </div>
+   )
  }
 
  export default SearchResult;
