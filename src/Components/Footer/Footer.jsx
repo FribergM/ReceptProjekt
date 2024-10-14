8a8958eeb7708b87feabac36e74c55d9a27ee3fa
LLLLLLisaa <xianochchu@gmail.com>
2024-10-14 03:29:16 +0200

    add tagline till footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,12 +15,34 @@ function Footer() {
 
     return (
         <footer className="footer">
-            <div className="footer-content">
+            {/* <div className="footer-content">
+                <h2 className ="footer__slogan"> - Mangia, bevi, e sii felice!</h2>
                 <p>&copy; {new Date().getFullYear()} Mangia. All rights reserved.</p>
                 <div className="footer-links">
                     <a href="#privacy-policy" onClick={() => openModal('privacy')}>Integritetspolicy</a>
                     <a href="#terms-of-service" onClick={() => openModal('terms')}>Användarvillkor</a>
                     <a href="#contact" onClick={() => openModal('contact')}>Kontakta oss</a>
+                </div>  
+            </div> */}
+
+
+
+               <div className="footer-content">
+                 <div className ="footer-top">
+                 <p>&copy; {new Date().getFullYear()} Mangia. All rights reserved.</p>
+                <div className="footer-links">
+                    <a href="#privacy-policy" onClick={() => openModal('privacy')}>Integritetspolicy</a>
+                    <a href="#terms-of-service" onClick={() => openModal('terms')}>Användarvillkor</a>
+                    <a href="#contact" onClick={() => openModal('contact')}>Kontakta oss</a>
+                 </div>
+
+                 <div className="footer-divider"></div>
+                 
+                 <div className="footer-bottom">
+                   <div className="footer__slogan"> - Mangia, bevi, e sii felice!
+                 </div>
+                 </div>
+                
                 </div>  
             </div>
 
