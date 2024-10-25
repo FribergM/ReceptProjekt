c7527255681d669892d3b34c41ae92fbdbf60e5d
Petter Lundqvist <116175650+Lndqvist@users.noreply.github.com>
2024-10-25 14:43:55 +0200

    Minor changes to ensure the recipe cards have the same height

--- src/Components/RecipeCarousel/RecipeCarousel.jsx
+++ src/Components/RecipeCarousel/RecipeCarousel.jsx
@@ -21,21 +21,15 @@ const RecipeCarousel = ({ recipes }) => {
         }
       },
       {
-        breakpoint: 840,
+        breakpoint: 1030,
         settings: {
           slidesToShow: 2, 
           slidesToScroll: 2,
         }
       },
+      
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3, 
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 1440,
+        breakpoint: 1400,
         settings: {
           slidesToShow: 3, 
           slidesToScroll: 3,
