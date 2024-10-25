b78b352dc8458a7f1e51664d1e1aba02fbf0354e
Petter Lundqvist <116175650+Lndqvist@users.noreply.github.com>
2024-10-25 15:28:06 +0200

    Replaced header text and background

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,11 @@ function Header() {
         <Link to={`/`} className="header-link">
             <div className="header-container">
                 <header className="header">
-                    <div className="header-text">
-                        <h1 className="header__name">MANGIA</h1>
-                        <h2 className="header__slogan">- Mangia, bevi, e sii felice!</h2>
+                    <div className="header-logo-container">
+                        <img src="/images/mangia-logo.svg" alt="mangia-logo" className="header-logo"/> 
                     </div>
+                     
+                    
 
                 </header>
             </div>  
